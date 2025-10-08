b6c00ead5cd75ebb09d95c5d577addb62a151070
hao rui <194741615+haorui2004@users.noreply.github.com>
2025-10-09 00:35:24 +0800

    Add a parser class for CancelLessonCommand

--- src/main/java/seedu/address/logic/parser/CancelLessonCommandParser.java
+++ src/main/java/seedu/address/logic/parser/CancelLessonCommandParser.java
@@ -1,4 +1,35 @@
 package seedu.address.logic.parser;
 
-public class CancelLessonCommandParser {
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.CancelLessonCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+/**
+ * Parses input arguments and creates a new CancelLessonCommand object
+ */
+public class CancelLessonCommandParser implements Parser<CancelLessonCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the CancelLessonCommand
+     * and returns an CancelLessonCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public CancelLessonCommand parse(String args) throws ParseException {
+        requireNonNull(args);
+
+        Index index;
+
+        try {
+            index = ParserUtil.parseIndex(args.trim());
+        } catch (ParseException pe) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, CancelLessonCommand.MESSAGE_USAGE), pe
+            );
+        }
+
+        return new CancelLessonCommand(index);
+    }
 }
