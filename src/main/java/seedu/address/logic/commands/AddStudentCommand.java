4de54d804dc7456effaeee977a9b1abcdc4c219f
Anxinal <ax1375240926@gmail.com>
2025-10-08 18:12:36 +0800

    Initialise the add student class

---
+++ src/main/java/seedu/address/logic/commands/AddStudentCommand.java
@@ -0,0 +1,32 @@
+package seedu.address.logic.commands;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+import seedu.address.model.person.Student;
+
+public class AddStudentCommand extends AddCommand {
+
+
+    public static final String COMMAND_WORD = "addstu";
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a student to the address book. "
+            + "Parameters: "
+            + PREFIX_NAME + "NAME "
+            + PREFIX_PHONE + "PHONE "
+            + PREFIX_EMAIL + "EMAIL "
+            + PREFIX_ADDRESS + "ADDRESS "
+            + "[" + PREFIX_TAG + "TAG]...\n"
+            + "Example: " + COMMAND_WORD + " "
+            + PREFIX_NAME + "Alice Pauline"
+            + PREFIX_PHONE + "92345123 "
+            + PREFIX_EMAIL + "1200033@gmail.com";
+    public static final String MESSAGE_SUCCESS = "New person added: %1$s";
+    public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in the address book";
+
+
+    public AddStudentCommand(Student student) {
+        super(student);
+    }
+    
+}
