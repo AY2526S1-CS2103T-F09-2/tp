a7a6781855b02d54e9ca9a69abe549b6d2e22807
Anxinal <ax1375240926@gmail.com>
2025-10-08 22:07:12 +0800

    Add some test cases for add student command

--- src/main/java/seedu/address/logic/commands/AddStudentCommand.java
+++ src/main/java/seedu/address/logic/commands/AddStudentCommand.java
@@ -1,12 +1,19 @@
 package seedu.address.logic.commands;
+import static java.util.Objects.requireNonNull;
+
+import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.logic.Messages;
+import seedu.address.logic.commands.exceptions.CommandException;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+import seedu.address.model.Model;
 import seedu.address.model.person.Student;
 
-public class AddStudentCommand extends AddCommand {
+
+public class AddStudentCommand extends Command {
 
     
     public static final String COMMAND_WORD = "addstu";
@@ -21,12 +28,52 @@ public class AddStudentCommand extends AddCommand {
             + PREFIX_NAME + "Alice Pauline"
             + PREFIX_PHONE + "92345123 "
             + PREFIX_EMAIL + "1200033@gmail.com";
-    public static final String MESSAGE_SUCCESS = "New person added: %1$s";
-    public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in the address book";
+    public static final String MESSAGE_SUCCESS = "New Student added: %1$s";
+    public static final String MESSAGE_DUPLICATE_STUDENT = "This Student already exists in the address book";
 
 
+    private final Student toAdd;
+
+    /**
+     * Creates an AddCommand to add the specified {@code Student}
+     */
     public AddStudentCommand(Student student) {
-        super(student);
+        requireNonNull(student);
+        toAdd = student;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+
+        if (model.hasPerson(toAdd)) {
+            throw new CommandException(MESSAGE_DUPLICATE_STUDENT);
+        }
+
+        model.addPerson(toAdd);
+        return new CommandResult(String.format(MESSAGE_SUCCESS, Messages.format(toAdd)));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof AddStudentCommand)) {
+            return false;
+        }
+
+        AddStudentCommand otherAddCommand = (AddStudentCommand) other;
+        return toAdd.equals(otherAddCommand.toAdd);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("toAdd", toAdd)
+                .toString();
     }
     
 }
