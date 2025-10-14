226b30dd739e77394fba793c8582ad01a55fb366
vinncee <vince6921chua@gmail.com>
2025-10-14 23:48:00 +0800

    Fix checkstyle

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -17,12 +17,11 @@ import seedu.address.model.person.Student;
 public class PersonCard extends UiPart<Region> {
 
     private static final String FXML = "PersonListCard.fxml";
+    public final Person person;
     @FXML
     private Label lessonLabel;
     @FXML
     private Label paymentStatusLabel;
-
-
     /**
      * Note: Certain keywords such as "location" and "resources" are reserved keywords in JavaFX.
      * As a consequence, UI elements' variable names cannot be set to such keywords
@@ -30,9 +29,6 @@ public class PersonCard extends UiPart<Region> {
      *
      * @see <a href="https://github.com/se-edu/addressbook-level4/issues/336">The issue on AddressBook level 4</a>
      */
-
-    public final Person person;
-
     @FXML
     private HBox cardPane;
     @FXML
@@ -47,7 +43,6 @@ public class PersonCard extends UiPart<Region> {
     private Label email;
     @FXML
     private FlowPane tags;
-
     /**
      * Creates a {@code PersonCode} with the given {@code Person} and index to display.
      */
