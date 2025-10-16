2b08446b0535c62bc24e00b8f9b513ee761b46f9
feliciaz05 <felicia.sgp@gmail.com>
2025-10-16 19:34:49 +0800

    Fix Lesson checkstyle

--- src/main/java/seedu/address/model/Lesson.java
+++ src/main/java/seedu/address/model/Lesson.java
@@ -5,22 +5,20 @@ package seedu.address.model;
  */
 public class Lesson {
 
+    public static final Lesson EMPTY = new EmptyLesson();
+
+    private String lessonDate;
+    private boolean isCompleted;
 
     /**
-     * Represents an empty Lesson.
-     * This is used when a Student has no upcoming lessons.
-     * This allows us to avoid using null to represent the absence of a Lesson.
+     * This method creates a new Lesson object
+     * @param lessonDate
      */
-    private static final class EmptyLesson extends Lesson {
-        private EmptyLesson() {
-            super("No upcoming lessons");
-        }
-
-        @Override
-        public boolean isEmpty() {
-            return true;
-        }
+    public Lesson(String lessonDate) {
+        this.lessonDate = lessonDate;
+        this.isCompleted = false;
     }
+
     /**
     * Returns true if this lesson is empty (i.e., Lesson.EMPTY)
     */
@@ -28,18 +26,6 @@ public class Lesson {
         return "No upcoming lessons".equals(lessonDate) || lessonDate == null || lessonDate.trim().isEmpty();
     }
 
-    public static final Lesson EMPTY = new EmptyLesson();
-    private String lessonDate;
-    private boolean isCompleted;
-
-    /**
-    * This method creates a new Lesson object
-    * @param lessonDate
-    */
-    public Lesson(String lessonDate) {
-        this.lessonDate = lessonDate;
-        this.isCompleted = false;
-    }
     /**
      * This method returns the date of the specific lesson of the student
      * @return String
@@ -59,6 +45,22 @@ public class Lesson {
      */
     public boolean isCompleted() {
         return isCompleted;
+    }
+
+    /**
+     * Represents an empty Lesson.
+     * This is used when a Student has no upcoming lessons.
+     * This allows us to avoid using null to represent the absence of a Lesson.
+     */
+    private static final class EmptyLesson extends Lesson {
+        private EmptyLesson() {
+            super("No upcoming lessons");
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return true;
+        }
     }
 }
 
