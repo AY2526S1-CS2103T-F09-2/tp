c56684e36f05021d1913092528745f60ca232d46
Anxinal <ax1375240926@gmail.com>
2025-10-22 17:03:57 +0800

    Import order issue

--- src/test/java/seedu/address/logic/commands/CancelLessonCommandTest.java
+++ src/test/java/seedu/address/logic/commands/CancelLessonCommandTest.java
@@ -3,14 +3,18 @@ package seedu.address.logic.commands;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.commands.CommandTestUtil.showPersonAtIndex;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.Messages;
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.logic.commands.CommandTestUtil.showPersonAtIndex;
 import seedu.address.model.AddressBook;
 import seedu.address.model.Lesson;
 import seedu.address.model.Model;
@@ -20,9 +24,7 @@ import seedu.address.model.person.Person;
 import seedu.address.model.person.Student;
 import seedu.address.testutil.PersonBuilder;
 import seedu.address.testutil.StudentBuilder;
-import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
-import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
-import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
 
 public class CancelLessonCommandTest {
     private Model model;
