c7428208438a49ab531aaa1d1f3f43fd5957e98a
vinncee <vince6921chua@gmail.com>
2025-10-22 14:35:14 +0800

    Update toString

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -154,6 +154,7 @@ public class Person {
                 .add("email", email)
                 .add("address", address)
                 .add("tags", tags)
+                .add("educationLevel", educationLevel)
                 .toString();
     }
 
