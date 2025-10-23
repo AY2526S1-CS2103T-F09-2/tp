40b55a8408fe1c657e2d2f39e2cb54ea0c715ffa
hao rui <194741615+haorui2004@users.noreply.github.com>
2025-10-23 21:18:59 +0800

    Update the example usage of addLesson to show a recurring example

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -13,6 +13,7 @@ public class CliSyntax {
     public static final Prefix PREFIX_TAG = new Prefix("t/");
     public static final Prefix PREFIX_LESSON = new Prefix("l/");
     public static final Prefix PREFIX_PAYMENT_STATUS = new Prefix("s/");
+    public static final Prefix PREFIX_INTERVAL = new Prefix("every/");
 
 }
 
